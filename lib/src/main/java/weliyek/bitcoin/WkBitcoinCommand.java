57f662875626e3fcc9849da1fa7c2a6b1d821b35
Ricardo Villalobos <pocolocosv@pm.me>
2023-08-18 17:07:11 -0600

    Refactoring and adding test for bitcoin command type.
    Removing all references to the type WkBitcoinCommandName that is no
    longer needed here. That type will be removed later on from the project.

--- lib/src/main/java/weliyek/bitcoin/WkBitcoinCommand.java
+++ lib/src/main/java/weliyek/bitcoin/WkBitcoinCommand.java
@@ -17,22 +17,26 @@
  */
 package weliyek.bitcoin;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
+import java.util.regex.Pattern;
 
 import weliyek.util.array.WkByteArray;
 
 public class WkBitcoinCommand
 {
 
-    public static WkBitcoinCommand newCommand(byte[] array, int offset, int length) {
-        byte[] array_copy = Arrays.copyOfRange(array, offset, offset + length);
+    public static WkBitcoinCommand newCommand(byte[] array) {
+      return newCommand(array, 0);
+    }
+
+    public static WkBitcoinCommand newCommand(byte[] array, int offset) {
+        if (CANONICAL_LENGTH != (array.length - offset))
+          throw new IllegalArgumentException();
+        byte[] array_copy = Arrays.copyOfRange(array, offset, array.length - offset);
         WkByteArray wrapper = new WkByteArray(array_copy);
         return newCommand(wrapper);
     }
@@ -40,6 +44,7 @@ public class WkBitcoinCommand
     public static WkBitcoinCommand newCommand(WkByteArray wrapper) {
         if (CANONICAL_LENGTH != wrapper.getLength())
             throw new IllegalArgumentException();
+        // Get existing instance if possible.
         WkBitcoinCommand cmd = CMD_BY_NAME.get(wrapper);
         if (null == cmd)
             cmd = new WkBitcoinCommand(wrapper);
@@ -47,42 +52,47 @@ public class WkBitcoinCommand
     }
 
     public static byte[] asCanonicalByteArray(String str) {
-        return Arrays.copyOf(str.getBytes(StandardCharsets.UTF_8), CANONICAL_LENGTH);
+        return Arrays.copyOf(str.getBytes(StandardCharsets.US_ASCII), CANONICAL_LENGTH);
     }
 
     public static final int CANONICAL_LENGTH = 12;
 
-    public static final WkBitcoinCommand VERSION     = new WkBitcoinCommand(WkBitcoinCommandName.VERSION);
-    public static final WkBitcoinCommand VERACK      = new WkBitcoinCommand(WkBitcoinCommandName.VERACK);
-    public static final WkBitcoinCommand ADDR        = new WkBitcoinCommand(WkBitcoinCommandName.ADDR);
-    public static final WkBitcoinCommand INV         = new WkBitcoinCommand(WkBitcoinCommandName.INV);
-    public static final WkBitcoinCommand GETDATA     = new WkBitcoinCommand(WkBitcoinCommandName.GETDATA);
-    public static final WkBitcoinCommand NOTFOUND    = new WkBitcoinCommand(WkBitcoinCommandName.NOTFOUND);
-    public static final WkBitcoinCommand GETBLOCKS   = new WkBitcoinCommand(WkBitcoinCommandName.GETBLOCKS);
-    public static final WkBitcoinCommand GETHEADERS  = new WkBitcoinCommand(WkBitcoinCommandName.GETHEADERS);
-    public static final WkBitcoinCommand TX          = new WkBitcoinCommand(WkBitcoinCommandName.TX);
-    public static final WkBitcoinCommand BLOCK       = new WkBitcoinCommand(WkBitcoinCommandName.BLOCK);
-    public static final WkBitcoinCommand HEADERS     = new WkBitcoinCommand(WkBitcoinCommandName.HEADERS);
-    public static final WkBitcoinCommand GETADDR     = new WkBitcoinCommand(WkBitcoinCommandName.GETADDR);
-    public static final WkBitcoinCommand MEMPOOL     = new WkBitcoinCommand(WkBitcoinCommandName.MEMPOOL);
-    public static final WkBitcoinCommand CHECKORDER  = new WkBitcoinCommand(WkBitcoinCommandName.CHECKORDER);
-    public static final WkBitcoinCommand SUBMITORDER = new WkBitcoinCommand(WkBitcoinCommandName.SUBMITORDER);
-    public static final WkBitcoinCommand REPLY       = new WkBitcoinCommand(WkBitcoinCommandName.REPLY);
-    public static final WkBitcoinCommand PING        = new WkBitcoinCommand(WkBitcoinCommandName.PING);
-    public static final WkBitcoinCommand PONG        = new WkBitcoinCommand(WkBitcoinCommandName.PONG);
-    public static final WkBitcoinCommand REJECT      = new WkBitcoinCommand(WkBitcoinCommandName.REJECT);
-    public static final WkBitcoinCommand FILTERLOAD  = new WkBitcoinCommand(WkBitcoinCommandName.FILTERLOAD);
-    public static final WkBitcoinCommand FILTERADD   = new WkBitcoinCommand(WkBitcoinCommandName.FILTERADD);
-    public static final WkBitcoinCommand FILTERCLEAR = new WkBitcoinCommand(WkBitcoinCommandName.FILTERCLEAR);
-    public static final WkBitcoinCommand MERKLEBLOCK = new WkBitcoinCommand(WkBitcoinCommandName.MERKLEBLOCK);
-    public static final WkBitcoinCommand ALERT       = new WkBitcoinCommand(WkBitcoinCommandName.ALERT);
-    public static final WkBitcoinCommand SENDHEADERS = new WkBitcoinCommand(WkBitcoinCommandName.SENDHEADERS);
-    public static final WkBitcoinCommand FEEFILTER   = new WkBitcoinCommand(WkBitcoinCommandName.FEEFILTER);
-    public static final WkBitcoinCommand SENDCMPCT   = new WkBitcoinCommand(WkBitcoinCommandName.SENDCMPCT);
-    public static final WkBitcoinCommand CMPCTBLOCK  = new WkBitcoinCommand(WkBitcoinCommandName.CMPCTBLOCK);
-    public static final WkBitcoinCommand GETBLOCKTXN = new WkBitcoinCommand(WkBitcoinCommandName.GETBLOCKTXN);
-    public static final WkBitcoinCommand BLOCKTXN    = new WkBitcoinCommand(WkBitcoinCommandName.BLOCKTXN);
+    public static final WkBitcoinCommand VERSION     = new WkBitcoinCommand("version");
+    public static final WkBitcoinCommand VERACK      = new WkBitcoinCommand("verack");
+    public static final WkBitcoinCommand ADDR        = new WkBitcoinCommand("addr");
+    public static final WkBitcoinCommand INV         = new WkBitcoinCommand("inv");
+    public static final WkBitcoinCommand GETDATA     = new WkBitcoinCommand("getdata");
+    public static final WkBitcoinCommand NOTFOUND    = new WkBitcoinCommand("notfound");
+    public static final WkBitcoinCommand GETBLOCKS   = new WkBitcoinCommand("getblocks");
+    public static final WkBitcoinCommand GETHEADERS  = new WkBitcoinCommand("getheaders");
+    public static final WkBitcoinCommand TX          = new WkBitcoinCommand("tx");
+    public static final WkBitcoinCommand BLOCK       = new WkBitcoinCommand("block");
+    public static final WkBitcoinCommand HEADERS     = new WkBitcoinCommand("headers");
+    public static final WkBitcoinCommand GETADDR     = new WkBitcoinCommand("getaddr");
+    public static final WkBitcoinCommand MEMPOOL     = new WkBitcoinCommand("mempool");
+    public static final WkBitcoinCommand CHECKORDER  = new WkBitcoinCommand("checkorder");
+    public static final WkBitcoinCommand SUBMITORDER = new WkBitcoinCommand("submitorder");
+    public static final WkBitcoinCommand REPLY       = new WkBitcoinCommand("reply");
+    public static final WkBitcoinCommand PING        = new WkBitcoinCommand("ping");
+    public static final WkBitcoinCommand PONG        = new WkBitcoinCommand("pong");
+    public static final WkBitcoinCommand REJECT      = new WkBitcoinCommand("reject");
+    public static final WkBitcoinCommand FILTERLOAD  = new WkBitcoinCommand("filterload");
+    public static final WkBitcoinCommand FILTERADD   = new WkBitcoinCommand("filteradd");
+    public static final WkBitcoinCommand FILTERCLEAR = new WkBitcoinCommand("filterclear");
+    public static final WkBitcoinCommand MERKLEBLOCK = new WkBitcoinCommand("merkleblock");
+    public static final WkBitcoinCommand ALERT       = new WkBitcoinCommand("alert");
+    public static final WkBitcoinCommand SENDHEADERS = new WkBitcoinCommand("sendheaders");
+    public static final WkBitcoinCommand FEEFILTER   = new WkBitcoinCommand("feefilter");
+    public static final WkBitcoinCommand SENDCMPCT   = new WkBitcoinCommand("sendcmpct");
+    public static final WkBitcoinCommand CMPCTBLOCK  = new WkBitcoinCommand("cmpctblock");
+    public static final WkBitcoinCommand GETBLOCKTXN = new WkBitcoinCommand("getblocktxn");
+    public static final WkBitcoinCommand BLOCKTXN    = new WkBitcoinCommand("blocktxn");
 
+    /**
+     * This table helps to obtain all the existing instances by their bytes.
+     * We use the type {@link WkByteArray} because most of the time it will
+     * likely be this type of object that will be used over the byte[]. 
+     */
     private static final Map<WkByteArray, WkBitcoinCommand> CMD_BY_NAME;
 
     static {
@@ -119,58 +129,33 @@ public class WkBitcoinCommand
         CMD_BY_NAME.put(BLOCKTXN.bytes,    BLOCKTXN);
     }
 
-    public final Optional<WkBitcoinCommandName> name;
-
     public final WkByteArray bytes;
+    public final String name;
 
-    /**
-     * Cached hash code.
+    /*
+     * Creates a new command using the ASCII version of the input string.
      */
-    private final int hashCode;
-
-    /**
-     * Creates a new instance from the {@link WkBitcoinCommandName} string value. The bytes are
-     * generated from the string value.
-     *
-     * @param name Is the already defined Bitcoin commmand name.
-     */
-    private WkBitcoinCommand(WkBitcoinCommandName name) {
-        this.name = Optional.of(name);
-        final byte[] buff = asCanonicalByteArray(name.text);
-        this.bytes  = new WkByteArray(buff);
-        this.hashCode = computeHashCode();
+    private WkBitcoinCommand(String str) {
+      this(new WkByteArray(asCanonicalByteArray(str)));
     }
 
-    /**
-     * Creates a new instance without an associated Bitcoin command.
-     *
-     * @param wrapper Is the byte array wrapper with the command bytes.
-     */
     private WkBitcoinCommand(WkByteArray wrapper) {
-        this.name = Optional.empty();
         this.bytes = Objects.requireNonNull(wrapper);
-        this.hashCode = computeHashCode();
+        this.name = Pattern.compile("\\p{C}").matcher(bytes.convertToString(StandardCharsets.US_ASCII)).replaceAll("?");
     }
 
     @Override
     public String toString() {
-        if (name.isPresent())
-            return name.get().name();
-        else
-            return "UNKNOWN";
+      return this.name;
     }
 
     @Override
     public int hashCode() {
-        return this.hashCode;
-    }
-
-    private int computeHashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (name.isPresent() ? name.hashCode() : 0);
-        result = prime * result + bytes.hashCode();
-        return result;
+      final int prime = 31;
+      int result = 1;
+      result += prime * result + bytes.hashCode();
+      result += prime * result + name.hashCode();
+      return result;
     }
 
     @Override
@@ -184,48 +169,7 @@ public class WkBitcoinCommand
         WkBitcoinCommand other = (WkBitcoinCommand) obj;
         if ( ! bytes.equals(other.bytes))
             return false;
-        if ( ! name.isPresent()) {
-            if (other.name.isPresent())
-                return false;
-        } else if (!name.get().equals(other.name.orElse(null)))
-            return false;
         return true;
-    }
-
-    /* ========================================================================================= */
-
-    /* Retrieved from: https://stackoverflow.com/a/9855338 */
-    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-
-    public static String bytesToHex(byte[] bytes, int from, int to) {
-        PrintStream out = new PrintStream(new ByteArrayOutputStream());
-        bytesToHex(out, bytes, from ,to);
-        return out.toString();
-    }
-
-    /**
-     *
-     * @param out
-     * @param bytes
-     * @param from Is the byte position of the first byte to be processed inclusively.
-     * @param to Is the position after the last byte to be printed (i.e., exclusively).
-     */
-    public static void bytesToHex(PrintStream out, byte[] bytes, int from, int to) {
-        if (    (from > to)
-             || (from < 0)
-             || (to < 0)
-             || (from >= bytes.length)
-             || (to > bytes.length)) {
-            throw new IndexOutOfBoundsException();
-        }
-        char nibble = 0;
-        for ( int j = from; j < to; j++ ) {
-            int v = bytes[j] & 0xFF;
-            nibble = hexArray[v >>> 4];
-            out.print(nibble);
-            nibble = hexArray[v & 0x0F];
-            out.print(nibble);
-        }
     }
 
 }
---
+++ lib/src/test/java/weliyek/bitcoin/WkBitcoinCommandTest.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright (C) 2023  Ricardo Villalobos - All Rights Reserved
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package weliyek.bitcoin;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WkBitcoinCommandTest
+{
+
+  @Test
+  void testTooShortByteStream() {
+    byte[] bytesarray = new byte[] {'t','o','o','s','h','o','r','t',0};
+    assertThrows(IllegalArgumentException.class, () -> WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testTooLongByteStream() {
+    byte[] bytesarray = new byte[] {'t','o','o','l','o','n','g',0,0,0,0,0,0};
+    assertThrows(IllegalArgumentException.class, () -> WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testVersion() {
+    byte[] bytesarray = new byte[] {'v','e','r','s','i','o','n',0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.VERSION.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.VERSION, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testVerack() {
+    byte[] bytesarray = new byte[] {'v','e','r','a','c','k',0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.VERACK.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.VERACK, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testAddr() {
+    byte[] bytesarray = new byte[] {'a','d','d','r',0,0,0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.ADDR.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.ADDR, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testInv() {
+    byte[] bytesarray = new byte[] {'i','n','v',0,0,0,0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.INV.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.INV, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testGetdata() {
+    byte[] bytesarray = new byte[] {'g','e','t','d','a','t','a',0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.GETDATA.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.GETDATA, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testNotfound() {
+    byte[] bytesarray = new byte[] {'n','o','t','f','o','u','n','d',0,0,0,0};
+    assertTrue(WkBitcoinCommand.NOTFOUND.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.NOTFOUND, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testGetblocks() {
+    byte[] bytesarray = new byte[] {'g','e','t','b','l','o','c','k','s',0,0,0};
+    assertTrue(WkBitcoinCommand.GETBLOCKS.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.GETBLOCKS, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testGetheaders() {
+    byte[] bytesarray = new byte[] {'g','e','t','h','e','a','d','e','r','s',0,0};
+    assertTrue(WkBitcoinCommand.GETHEADERS.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.GETHEADERS, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testTx() {
+    byte[] bytesarray = new byte[] {'t','x',0,0,0,0,0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.TX.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.TX, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testBlock() {
+    byte[] bytesarray = new byte[] {'b','l','o','c','k',0,0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.BLOCK.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.BLOCK, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testHeaders() {
+    byte[] bytesarray = new byte[] {'h','e','a','d','e','r','s',0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.HEADERS.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.HEADERS, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testGetaddr() {
+    byte[] bytesarray = new byte[] {'g','e','t','a','d','d','r',0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.GETADDR.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.GETADDR, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testMempool() {
+    byte[] bytesarray = new byte[] {'m','e','m','p','o','o','l',0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.MEMPOOL.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.MEMPOOL, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testCheckorder() {
+    byte[] bytesarray = new byte[] {'c','h','e','c','k','o','r','d','e','r',0,0};
+    assertTrue(WkBitcoinCommand.CHECKORDER.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.CHECKORDER, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testSubmitorder() {
+    byte[] bytesarray = new byte[] {'s','u','b','m','i','t','o','r','d','e','r',0};
+    assertTrue(WkBitcoinCommand.SUBMITORDER.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.SUBMITORDER, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testReply() {
+    byte[] bytesarray = new byte[] {'r','e','p','l','y',0,0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.REPLY.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.REPLY, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testPing() {
+    byte[] bytesarray = new byte[] {'p','i','n','g',0,0,0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.PING.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.PING, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testPong() {
+    byte[] bytesarray = new byte[] {'p','o','n','g',0,0,0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.PONG.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.PONG, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testReject() {
+    byte[] bytesarray = new byte[] {'r','e','j','e','c','t',0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.REJECT.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.REJECT, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testFilterload() {
+    byte[] bytesarray = new byte[] {'f','i','l','t','e','r','l','o','a','d',0,0};
+    assertTrue(WkBitcoinCommand.FILTERLOAD.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.FILTERLOAD, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testFilteradd() {
+    byte[] bytesarray = new byte[] {'f','i','l','t','e','r','a','d','d',0,0,0};
+    assertTrue(WkBitcoinCommand.FILTERADD.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.FILTERADD, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testFilterclear() {
+    byte[] bytesarray = new byte[] {'f','i','l','t','e','r','c','l','e','a','r',0};
+    assertTrue(WkBitcoinCommand.FILTERCLEAR.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.FILTERCLEAR, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testMerkleblock() {
+    byte[] bytesarray = new byte[] {'m','e','r','k','l','e','b','l','o','c','k',0};
+    assertTrue(WkBitcoinCommand.MERKLEBLOCK.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.MERKLEBLOCK, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testAlert() {
+    byte[] bytesarray = new byte[] {'a','l','e','r','t',0,0,0,0,0,0,0};
+    assertTrue(WkBitcoinCommand.ALERT.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.ALERT, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testSendheaders() {
+    byte[] bytesarray = new byte[] {'s','e','n','d','h','e','a','d','e','r','s',0};
+    assertTrue(WkBitcoinCommand.SENDHEADERS.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.SENDHEADERS, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testFeefilter() {
+    byte[] bytesarray = new byte[] {'f','e','e','f','i','l','t','e','r',0,0,0};
+    assertTrue(WkBitcoinCommand.FEEFILTER.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.FEEFILTER, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testSendcmpct() {
+    byte[] bytesarray = new byte[] {'s','e','n','d','c','m','p','c','t',0,0,0};
+    assertTrue(WkBitcoinCommand.SENDCMPCT.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.SENDCMPCT, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testCmpctblock() {
+    byte[] bytesarray = new byte[] {'c','m','p','c','t','b','l','o','c','k',0,0};
+    assertTrue(WkBitcoinCommand.CMPCTBLOCK.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.CMPCTBLOCK, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testGetblocktxn() {
+    byte[] bytesarray = new byte[] {'g','e','t','b','l','o','c','k','t','x','n',0};
+    assertTrue(WkBitcoinCommand.GETBLOCKTXN.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.GETBLOCKTXN, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+  @Test
+  void testBlocktxn() {
+    byte[] bytesarray = new byte[] {'b','l','o','c','k','t','x','n',0,0,0,0};
+    assertTrue(WkBitcoinCommand.BLOCKTXN.bytes.equalsToArray(bytesarray));
+    assertEquals(WkBitcoinCommand.BLOCKTXN, WkBitcoinCommand.newCommand(bytesarray));
+  }
+
+}
